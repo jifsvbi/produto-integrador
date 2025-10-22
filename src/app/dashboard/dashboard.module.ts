f9c7b863c2b6f855b4cc22b038294589cab58001
jifsvbi <joaoprdro222931@gmsil.com>
2025-10-22 13:47:20 -0300

    gerenciando o Api e o proxy

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { DashboardPageRoutingModule } from './dashboard-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardPage } from './dashboard.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    DashboardPageRoutingModule,
+    HttpClientModule
+  ],
+  declarations: [DashboardPage]
+})
+export class DashboardPageModule {}
